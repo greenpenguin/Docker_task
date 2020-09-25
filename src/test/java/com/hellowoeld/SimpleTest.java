1b93e95c0b75667c0afc408f6e3baa33d7174523
Paleontolog <lezgyan@yandex.ru>
2020-09-25 12:15:35 +0400

    Add very simple test

---
+++ src/test/java/com/hellowoeld/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.hellowoeld;
+
+import org.junit.Test;
+import org.junit.gen5.api.Assertions;
+
+public class SimpleTest {
+    @Test
+    public void veryCleverTest() {
+        Assertions.assertEquals("Heresy", "Heresy");
+    }
+}
